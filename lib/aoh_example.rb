9c99c7e6c41ba12d910ecdf502db8648ae27b9a3
Skye Geraghty <skyetay03@googlemail.com>
2020-01-25 13:15:29 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,7 +26,7 @@ def literal_aoh
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
     { name:  "Kate Gompert", occupation: "None" },
-    BRUCE_G
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end
 
